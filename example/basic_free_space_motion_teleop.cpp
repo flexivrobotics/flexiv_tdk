90280130a150a21d65ea01876f6fe968654b23c5
Zhipeng Zhang <zhipeng.zhang@flexiv.com>
2023-11-23 15:51:32 +0800

    Modify hpp name

--- example/basic_free_space_motion_teleop.cpp
+++ example/basic_free_space_motion_teleop.cpp
@@ -1,27 +1,29 @@
 /**
  * @example basic_free_space_motion_teleop.cpp
  * @author flexiv
- * @brief basic free space motion robot to robot teleoperation using flexiv Rizon4s
+ * @brief basic free space motion robot to robot teleoperation using flexiv
+ * Rizon4s robots
  * @copyright Copyright (c) 2023
  */
 
 // Flexiv
-#include <flexiv/omni/teleop/Teleoperation.hpp>
+#include <chrono>
+#include <flexiv/omni/teleop/Robot2RobotTeleop.hpp>
 #include <getopt.h>
 #include <iostream>
 #include <thread>
-#include <chrono>
 
 using namespace flexiv;
+
 /** @brief Print usage of example*/
 void printHelp()
 {
-  // clang-format off
+    // clang-format off
     std::cout<<"Invalid program arguments"<<std::endl;
     std::cout<<"     -l     [necessary] serial number of local robot."<<std::endl;
     std::cout<<"     -r     [necessary] serial number of remote robot."<<std::endl;
     std::cout<<"Usage: ./test_flexiv_omni_teleop -l Rizon4s-123456 -r Rizon4s-654321"<<std::endl;
-  // clang-format on
+    // clang-format on
 }
 
 struct option k_longOptions[] = {
@@ -34,52 +36,52 @@ struct option k_longOptions[] = {
 
 int main(int argc, char* argv[])
 {
-  std::string remoteSN;
-  std::string localSN;
-  bool isBlocking = false;
-  int opt = 0;
-  int longIndex = 0;
-  while ((opt = getopt_long_only(argc, argv, "", k_longOptions, &longIndex)) != -1) {
-    switch (opt) {
-      case 'r':
-        remoteSN = std::string(optarg);
-        std::cout << "Remote SN: " + remoteSN << std::endl;
-        break;
-      case 'l':
-        localSN = std::string(optarg);
-        std::cout << "Local SN: " + localSN << std::endl;
-        break;
-      default:
+    std::string remoteSN;
+    std::string localSN;
+    bool isBlocking = false;
+    int opt = 0;
+    int longIndex = 0;
+    while ((opt = getopt_long_only(argc, argv, "", k_longOptions, &longIndex)) != -1) {
+        switch (opt) {
+            case 'r':
+                remoteSN = std::string(optarg);
+                std::cout << "Remote SN: " + remoteSN << std::endl;
+                break;
+            case 'l':
+                localSN = std::string(optarg);
+                std::cout << "Local SN: " + localSN << std::endl;
+                break;
+            default:
+                printHelp();
+                return 1;
+        }
+    }
+    if (localSN.empty() || remoteSN.empty()) {
         printHelp();
         return 1;
     }
-  }
-  if (localSN.empty() || remoteSN.empty()) {
-    printHelp();
-    return 1;
-  }
 
-  try {
+    try {
 
-    flexiv::omni::teleop::Teleoperation teleop(localSN, remoteSN);
+        flexiv::omni::teleop::Robot2RobotTeleop teleop(localSN, remoteSN);
 
-    // Enable teleop robots
-    teleop.enable();
+        // Enable teleop robots
+        teleop.enable();
 
-    // Init teleop robots
-    teleop.init();
+        // Init teleop robots
+        teleop.init();
 
-    // Run teleop
-    teleop.run(isBlocking);
+        // Run teleop
+        teleop.run(isBlocking);
 
-    while (true) {
-      std::cout << "Omni-Teleop is running in background... " << std::endl;
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+        while (true) {
+            std::cout << "Omni-Teleop is running in background... " << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return 1;
     }
-  } catch (const std::exception& e) {
-    std::cout << e.what() << std::endl;
-    return 1;
-  }
 
-  return 0;
+    return 0;
 }
