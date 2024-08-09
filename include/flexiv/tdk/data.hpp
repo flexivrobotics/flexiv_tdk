5ff3d53211e98d67d427af979988b1b0618e7c6e
Zhipeng Zhang <zhipeng.zhang@flexiv.com>
2024-08-09 18:01:32 +0800

    update headers

--- include/flexiv/tdk/data.hpp
+++ include/flexiv/tdk/data.hpp
@@ -16,9 +16,9 @@ namespace tdk {
  */
 enum CoordType
 {
-  COORD_UNKNOWN = 0, ///> Unknown coordinate
-  COORD_TCP,         ///> TCP coordinate of local robot
-  COORD_WORLD        ///> WORLD coordinate of local robot
+    COORD_UNKNOWN = 0, ///> Unknown coordinate
+    COORD_TCP,         ///> TCP coordinate of local robot
+    COORD_WORLD        ///> WORLD coordinate of local robot
 };
 
 static const std::string CoordTypeStr[] = {"UNKNOWN", "TCP", "WORLD"};
@@ -30,36 +30,36 @@ static const std::string CoordTypeStr[] = {"UNKNOWN", "TCP", "WORLD"};
  */
 static inline CoordType GetCoordType(const std::string& str)
 {
-  for (size_t i = 0; i < COORD_WORLD - COORD_UNKNOWN + 1; i++) {
-    if (str == CoordTypeStr[i]) {
-      return static_cast<CoordType>(i);
+    for (size_t i = 0; i < COORD_WORLD - COORD_UNKNOWN + 1; i++) {
+        if (str == CoordTypeStr[i]) {
+            return static_cast<CoordType>(i);
+        }
     }
-  }
-  return COORD_UNKNOWN;
+    return COORD_UNKNOWN;
 }
 
 /**
  * @brief Data for locking axis, including reference frame and axis to be locked.
  * Coordinate type options are: "COORD_TCP" for TCP frame and "COORD_WORLD" for WORLD frame.
  */
-struct AxisLockStatus
+struct AxisLock
 {
-  /**
-   * @brief Reference coordinate that the axis to be locked
-   */
-  CoordType coord = CoordType::COORD_UNKNOWN;
+    /**
+     * @brief Reference coordinate that the axis to be locked
+     */
+    CoordType coord = CoordType::COORD_UNKNOWN;
 
-  /**
-   * @brief Translation axis lock, the corresponding axis order is \f$ [X, Y, Z] \f$. True
-   * for locking, false for floating.
-   */
-  std::array<bool, 3> lock_trans_axis = {false, false, false};
+    /**
+     * @brief Translation axis lock, the corresponding axis order is \f$ [X, Y, Z] \f$. True
+     * for locking, false for floating.
+     */
+    std::array<bool, 3> lock_trans_axis = {false, false, false};
 
-  /**
-   * @brief Orientation axis lock, the corresponding axis order is \f$ [Rx, Ry, Rz] \f$.
-   * True for locking, false for floating.
-   */
-  std::array<bool, 3> lock_ori_axis = {false, false, false};
+    /**
+     * @brief Orientation axis lock, the corresponding axis order is \f$ [Rx, Ry, Rz] \f$.
+     * True for locking, false for floating.
+     */
+    std::array<bool, 3> lock_ori_axis = {false, false, false};
 };
 
 } // namespace tdk
