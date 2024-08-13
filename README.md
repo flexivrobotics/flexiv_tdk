f431f4692ae4b4b49a07eec8fd0c77b2fc05d089
Zhipeng-flexiv <68000300+ZhipengZhang-flexiv@users.noreply.github.com>
2024-08-13 10:38:44 +0800

    Release/v1.1 (#4)
    Release note:
    https://github.com/flexivrobotics/flexiv_tdk/releases/tag/v1.1

--- README.md
+++ README.md
@@ -1,74 +1,75 @@
-# Flexiv Omni Teleop
+# Flexiv TDK
 
-![CMake Badge](https://github.com/flexivrobotics/flexiv_omni_teleop/actions/workflows/cmake.yml/badge.svg) ![Version](https://img.shields.io/badge/version-1.0-blue.svg) [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0.html)
+![CMake Badge](https://github.com/flexivrobotics/flexiv_tdk/actions/workflows/cmake.yml/badge.svg) ![Version](https://img.shields.io/badge/version-1.1-blue.svg) [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0.html)
 
-The Flexiv Omni Teleop is an SDK provides C++ APIs for developing complex and customized applications involving robot-robot or device-robot teleoperation. The supported devices are listed below.
+Flexiv TDK (Teleoperation Development Kit) is an SDK provides C++ APIs for developing complex and customized applications involving robot-robot or device-robot teleoperation. The supported devices are listed below.
 
 ## Supported devices
 
-| **Supported teleop type** | **Supported local machine** | **Supported remote machine** |
+| **Supported teleop type** | **Supported local devices** | **Supported remote devices** |
 | ------------------------- | --------------------------- | ---------------------------- |
 | robot-robot               | Rizon4s                     | Rizon4s                      |
 
 ## Compatibility
 
-| **Supported OS**     | **Supported processor** | **Supported language** | **Required compiler kit** |
-| -------------------- | ----------------------- | ---------------------- | ------------------------- |
-| Linux (Ubuntu 22.04) | x86_64                  | C++                    | build-essential           |
+| **Supported OS**           | **Supported processor** | **Supported language** | **Required compiler kit** |
+| -------------------------- | ----------------------- | ---------------------- | ------------------------- |
+| Linux (Ubuntu 20.04/22.04) | x86_64                  | C++                    | build-essential           |
 
-### Compile and install for Linux
+### Install on Linux
 
 1. In a new Terminal, install C++ compiler, libssl-dev, net-tools, Git, and CMake (with GUI) using the package manager:
 
-      sudo apt install build-essential libssl-dev net-tools git cmake cmake-qt-gui -y
+       sudo apt install build-essential libssl-dev net-tools git cmake cmake-qt-gui -y
 
-2. Choose a directory for installing ``flexiv_omni_teleop`` library and all its dependencies. For example, a new folder named ``teleop_install`` under the home directory.
+2. Choose a directory for installing ``flexiv_tdk`` library and all its dependencies. For example, a new folder named ``tdk_install`` under the home directory.
 
 3. Please ensure that your network connection is unobstructed. Then, in a new Terminal, run the provided script to compile and install all dependencies to the installation directory chosen in step 2:
 
-      cd flexiv_omni_teleop/thirdparty
-      bash build_and_install_dependencies.sh ~/teleop_install
+       cd flexiv_tdk/thirdparty
+       bash build_and_install_dependencies.sh ~/tdk_install
 
-4. In a new Terminal, use CMake to configure `flexiv_omni_teleop`:
+4. In a new Terminal, use CMake to configure `flexiv_tdk`:
 
-      cd flexiv_omni_teleop
-      mkdir build && cd build
-      cmake .. -DCMAKE_INSTALL_PREFIX=~/teleop_install
+       cd flexiv_tdk
+       mkdir build && cd build
+       cmake .. -DCMAKE_INSTALL_PREFIX=~/tdk_install
 
    NOTE: ``-D`` followed by ``CMAKE_INSTALL_PREFIX`` is a CMake parameter specifying the path of the chosen installation directory. Alternatively, this configuration step can also be done through CMake GUI.
 
-5. Compile and install `flexiv_omni_teleop` library:
+5. Install `flexiv_tdk` library:
 
-      cd flexiv_omni_teleop/build
-      cmake --build . --target install --config Release
+       cd flexiv_tdk/build
+       cmake --build . --target install --config Release
 
-   NOTE: the installation of `flexiv_omni_teleop` library is complete now. The following steps show how to link to the installed library from a user project.
+   NOTE: the installation of `flexiv_tdk` library is complete now. The following steps show how to link to the installed library from a user project.
 
 ### Link to the installed library to a user project
 
-1. To find and link to the installed `flexiv_omni_teleop` library to a user project, using the provided example project for instance:
+1. To find and link to the installed `flexiv_tdk` library to a user project, using the provided example project for instance:
 
-      cd flexiv_omni_teleop/example
-      mkdir build && cd build
-      cmake .. -DCMAKE_INSTALL_PREFIX=~/teleop_install
-      cmake --build . --config Release -j 4
+       cd flexiv_tdk/example
+       mkdir build && cd build
+       cmake .. -DCMAKE_INSTALL_PREFIX=~/tdk_install
+       cmake --build . --config Release -j 4
 
-   NOTE: ``-D`` followed by ``CMAKE_INSTALL_PREFIX`` tells user project's CMake where to find the installed `flexiv_omni_teleop` library.
+   NOTE: ``-D`` followed by ``CMAKE_INSTALL_PREFIX`` tells user project's CMake where to find the installed `flexiv_tdk` library.
 
-### Run Omni-Teleop
+### Run tdk example
 
-1. Apply for Omni license to run Omni-Teleop. See [Omni_license.md](omni_license_generator/Omni_license.md)
+1. Apply for license to run tdk. See [apply_for_license.md](./license_generator/apply_for_license.md)
 
-2. Set all the robots to `Auto/Remote Mode` via flexiv Elements, then to run the compiled example program:
+2. Set all the robots to `Auto/Remote Mode` via Flexiv Elements, then to run the compiled example program:
 
-      ./<program_name> [remote_robot_SN] [local_robot_SN] [path_to_omni_licenseCfg.json]
+       ./<program_name> [local_robot_serial_number] [remote_robot_serial_number] [path_to_omni_licenseCfg.json]
 
-### API Documentation
+
+## API Documentation
 
 API doc can be generated using doxygen
 
       sudo apt install doxygen-latex graphviz
-      cd flexiv_omni_teleop
-      doxygen doc/Doxygen.in
+      cd flexiv_tdk
+      doxygen doc/Doxyfile.in
 
-The generated API doc is under ```flexiv_omni_teleop/doc/html ```. You can open any of the html file with your web browser to view it.
+The generated API doc is under ```flexiv_tdk/doc/html ```. You can open any of the html file with your web browser to view it.
--- example/basics1_cartesian_teleop.cpp
+++ example/basics1_cartesian_teleop.cpp
@@ -1,64 +1,71 @@
 /**
- * @file basics1_cartesian_teleop.cpp
+ * @example basics1_cartesian_teleop.cpp
  * @copyright Copyright (C) 2016-2024 Flexiv Ltd. All Rights Reserved.
- * @brief Basic Omni-Teleop example
- * @date 2024-05-10
+ * @brief This is an example program for cartesian space teleoperation
+ * @author Flexiv
  */
 
-#include <flexiv/omni/teleop/Robot2RobotTeleop.hpp>
-#include <flexiv/omni/teleop/TeleopDefs.hpp>
-#include <flexiv/omni/teleop/Scheduler.hpp>
-#include <iostream>
-#include <thread>
-#include <chrono>
 #include <atomic>
+#include <chrono>
+#include <spdlog/spdlog.h>
+#include <flexiv/tdk/robot_robot_teleop.hpp>
+#include <flexiv/tdk/data.hpp>
+#include <flexiv/rdk/scheduler.hpp>
+#include <flexiv/rdk/data.hpp>
+#include <thread>
+#include <iostream>
 namespace {
-constexpr std::array<double, flexiv::omni::teleop::k_jointDOF> k_preferredJntPos
-    = {-0.67, -0.98, 0.89, 1.55, -0.85, 0.54, 0.46}; ///< Preferred joint position
-const std::array<double, flexiv::omni::teleop::k_cartDOF> k_defaultMaxRemoteWrench
-    = {80.0, 80.0, 80.0, 24.0, 24.0, 24.0}; ///< Maximum contact wrench of remote robot
-std::atomic<bool> g_stop_sched = {false};   ///< Atomic signal to stop scheduler tasks
-}
+std::vector<double> kPreferredJntPos
+    = {60 * M_PI / 180.0, -60 * M_PI / 180.0, -85 * M_PI / 180.0, 115 * M_PI / 180.0,
+        70 * M_PI / 180.0, 0 * M_PI / 180.0, 0 * M_PI / 180.0}; ///< Preferred joint position
 
+const std::array<double, flexiv::rdk::kCartDoF> kDefaultMaxContactWrench
+    = {200.0, 200.0, 200.0, 40.0, 40.0, 40.0}; ///< Maximum contact wrench
+
+std::atomic<bool> g_stop_sched = {false}; ///< Atomic signal to stop scheduler tasks
+} // namespace
+
+/**
+ * @brief Helper function to print program usage help
+ */
 void printHelp()
 {
     // clang-format off
-    std::cout << "Invalid program arguments"<<std::endl;
-    std::cout << "Required arguments: [remote_robot_SN] [local_robot_SN] [path_to_omni_licenseCfg.json]" << std::endl;
-    std::cout << "    remote_robot_SN: serial number of remote robot" << std::endl;
-    std::cout << "    local_robot_SN: serial number of local robot" << std::endl;
-    std::cout << "    path_to_omni_licenseCfg: path to the Omni license config json file" << std::endl;
-    std::cout<<"Usage: ./basic1_cart_teleop  Rizon4s-123456  Rizon4s-654321 <path/to/licensCfg.json>"<<std::endl;
+    spdlog::error("Invalid program arguments");
+    spdlog::info("  serial number of local robot");
+    spdlog::info("  serial number of remote robot");
+    spdlog::info("  license config file path");
+    spdlog::info("Usage: sudo ./basic1_cart_teleop Rizon4s-123456 Rizon4s-654321 <absolute path to licenseCfg.json>");
     // clang-format on
 }
 
 /**
- * @brief  Callback function for teleop task
+ * @brief 1000Hz callback function for main teleop task
  */
-void periodicTeleopTask(flexiv::omni::teleop::Robot2RobotTeleop& teleop)
+void PeriodicTeleopTask(flexiv::tdk::Robot2RobotTeleop& teleop)
 {
 
     try {
-        // Monitor status of the teleop robots
-        if (!teleop.isOperational()) {
+        // Monitor fault on the teleop robots
+        if (!teleop.operational()) {
             throw std::runtime_error(
-                "periodicTeleopTask: Fault occurred during teleoperation, exiting ...");
+                "PeriodicTeleopTask: Fault occurred during teleoperation, exiting ...");
         }
         // Step teleop
-        teleop.step();
+        teleop.Step();
     } catch (const std::exception& e) {
-        std::cerr << e.what() << '\n';
+        spdlog::error(e.what());
         g_stop_sched = true;
     }
 }
 
 /**
- * @brief Callback function for axis lock/unlock test
+ * @brief 1Hz callback function for several APIs call
  */
-void periodicConsoleTask(flexiv::omni::teleop::Robot2RobotTeleop& teleop)
+void PeriodicConsoleTask(flexiv::tdk::Robot2RobotTeleop& teleop)
 {
-    flexiv::omni::teleop::AxisLockDefs cmd;
-    teleop.getLocalAxisLockState(cmd);
+    flexiv::tdk::AxisLock cmd;
+    teleop.GetLocalAxisLockState(cmd);
 
     while (!g_stop_sched) {
 
@@ -66,182 +73,155 @@ void periodicConsoleTask(flexiv::omni::teleop::Robot2RobotTeleop& teleop)
         std::getline(std::cin, userInput);
 
         switch (userInput[0]) {
-            case 'm':
-                std::cout << ">>> Simple command line GUI for teleop robot axis lock <<<"
-                          << std::endl;
-                std::cout << "- x: lock/unlock translational motion along X axis in World frame."
-                          << std::endl;
-                std::cout << "- y: lock/unlock translational motion along Y axis in World frame."
-                          << std::endl;
-                std::cout << "- z: lock/unlock translational motion along Z axis in World frame."
-                          << std::endl;
-
-                std::cout << "- q: lock/unlock rotational motion along X axis in World frame."
-                          << std::endl;
-                std::cout << "- w: lock/unlock rotational motion along Y axis in World frame."
-                          << std::endl;
-                std::cout << "- e: lock/unlock rotational motion along Z axis in World frame."
-                          << std::endl;
-
-                std::cout << "- X: lock/unlock translational motion along X axis in TCP frame."
-                          << std::endl;
-                std::cout << "- Y: lock/unlock translational motion along Y axis in TCP frame."
-                          << std::endl;
-                std::cout << "- Z: lock/unlock translational motion along Z axis in TCP frame."
-                          << std::endl;
-
-                std::cout << "- Q: lock/unlock rotational motion along X axis in TCP frame."
-                          << std::endl;
-                std::cout << "- W: lock/unlock rotational motion along Y axis in TCP frame."
-                          << std::endl;
-                std::cout << "- E: lock/unlock rotational motion along Z axis in TCP frame."
-                          << std::endl;
-                std::cout << "please input command >> " << std::endl;
-                break;
             case 'x':
-                cmd.trans_axis_lock_list_[0] = !cmd.trans_axis_lock_list_[0];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_WORLD;
-
+                cmd.lock_trans_axis[0] = !cmd.lock_trans_axis[0];
+                cmd.coord = flexiv::tdk::CoordType::COORD_WORLD;
                 break;
             case 'y':
-                cmd.trans_axis_lock_list_[1] = !cmd.trans_axis_lock_list_[1];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_WORLD;
+                cmd.lock_trans_axis[1] = !cmd.lock_trans_axis[1];
+                cmd.coord = flexiv::tdk::CoordType::COORD_WORLD;
                 break;
             case 'z':
-                cmd.trans_axis_lock_list_[2] = !cmd.trans_axis_lock_list_[2];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_WORLD;
+                cmd.lock_trans_axis[2] = !cmd.lock_trans_axis[2];
+                cmd.coord = flexiv::tdk::CoordType::COORD_WORLD;
                 break;
             case 'q':
-                cmd.ori_axis_lock_list_[0] = !cmd.ori_axis_lock_list_[0];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_WORLD;
+                cmd.lock_ori_axis[0] = !cmd.lock_ori_axis[0];
+                cmd.coord = flexiv::tdk::CoordType::COORD_WORLD;
                 break;
             case 'w':
-                cmd.ori_axis_lock_list_[1] = !cmd.ori_axis_lock_list_[1];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_WORLD;
+                cmd.lock_ori_axis[1] = !cmd.lock_ori_axis[1];
+                cmd.coord = flexiv::tdk::CoordType::COORD_WORLD;
                 break;
             case 'e':
-                cmd.ori_axis_lock_list_[2] = !cmd.ori_axis_lock_list_[2];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_WORLD;
+                cmd.lock_ori_axis[2] = !cmd.lock_ori_axis[2];
+                cmd.coord = flexiv::tdk::CoordType::COORD_WORLD;
                 break;
 
             case 'X':
-                cmd.trans_axis_lock_list_[0] = !cmd.trans_axis_lock_list_[0];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_TCP;
+                cmd.lock_trans_axis[0] = !cmd.lock_trans_axis[0];
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
                 break;
             case 'Y':
-                cmd.trans_axis_lock_list_[1] = !cmd.trans_axis_lock_list_[1];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_TCP;
+                cmd.lock_trans_axis[1] = !cmd.lock_trans_axis[1];
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
                 break;
             case 'Z':
-                cmd.trans_axis_lock_list_[2] = !cmd.trans_axis_lock_list_[2];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_TCP;
+                cmd.lock_trans_axis[2] = !cmd.lock_trans_axis[2];
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
                 break;
             case 'Q':
-                cmd.ori_axis_lock_list_[0] = !cmd.ori_axis_lock_list_[0];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_TCP;
+                cmd.lock_ori_axis[0] = !cmd.lock_ori_axis[0];
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
                 break;
             case 'W':
-                cmd.ori_axis_lock_list_[1] = !cmd.ori_axis_lock_list_[1];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_TCP;
+                cmd.lock_ori_axis[1] = !cmd.lock_ori_axis[1];
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
                 break;
             case 'E':
-                cmd.ori_axis_lock_list_[2] = !cmd.ori_axis_lock_list_[2];
-                cmd.coord = flexiv::omni::teleop::CoordType::CD_TCP;
+                cmd.lock_ori_axis[2] = !cmd.lock_ori_axis[2];
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
                 break;
-
+            case 'r':
+                teleop.Engage(true);
+                break;
+            case 'R':
+                teleop.Engage(false);
+                break;
+            case 's':
+                cmd.lock_ori_axis = {false, false, false};
+                cmd.lock_trans_axis = {false, false, false};
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
+                break;
+            case 'S':
+                cmd.lock_ori_axis = {true, true, true};
+                cmd.lock_trans_axis = {true, true, true};
+                cmd.coord = flexiv::tdk::CoordType::COORD_TCP;
+                break;
+            case 't':
+                teleop.SetRepulsiveForce({0, 0, -5});
+                break;
+            case 'T':
+                teleop.SetWrenchFeedbackScalingFactor(0.5);
+                break;
             default:
-                std::cerr << "Invalid command, please enter \'m\' for help \n";
+                spdlog::warn("Invalid command!");
                 break;
         }
-        teleop.setLocalAxisLockCmd(cmd);
-        std::cout << " Axis [X] in " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << " frame locking status : " << cmd.trans_axis_lock_list_[0] << std::endl;
-        std::cout << " Axis [Y] in  " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << " frame locking status : " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << cmd.trans_axis_lock_list_[1] << std::endl;
-        std::cout << " Axis [Z] in  " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << " frame locking status : " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << cmd.trans_axis_lock_list_[2] << std::endl;
-        std::cout << " Axis [Rx] in  " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << " frame locking status : " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << cmd.ori_axis_lock_list_[0] << std::endl;
-        std::cout << " Axis [Ry] in  " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << " frame locking status : " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << cmd.ori_axis_lock_list_[1] << std::endl;
-        std::cout << " Axis [Rz] in  " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << " frame locking status : " << flexiv::omni::teleop::CoordTypeStr[cmd.coord]
-                  << cmd.ori_axis_lock_list_[2] << std::endl;
+        teleop.SetLocalAxisLockCmd(cmd);
     }
     return;
 }
 
 int main(int argc, char* argv[])
 {
-    std::string remoteSN;
-    std::string localSN;
-    std::string licCfgPath;
-
+    // Program Setup
+    // =============================================================================================
     if (argc != 4) {
         printHelp();
         return 1;
     }
-    remoteSN = argv[1];
-    localSN = argv[2];
-    licCfgPath = argv[3];
+    // Serial number of the local robot to connect to. Remove any space, for example: Rizon4s-062001
+    std::string local_sn = argv[1];
+    // Serial number of the remote robot to connect to. Remove any space, for example:
+    // Rizon4s-Rizon4s-062002
+    std::string remote_sn = argv[2];
+    // Path to license config json file. for example: ~/my_license/omni_licenseCfg.json
+    std::string path_to_lic_jsn = argv[3];
 
-    std::cout << "Flexiv Omni-Teleop example" << std::endl;
-    std::cout << "Copyright (C) 2016-2024 Flexiv" << std::endl;
-    std::cout << "All Rights Reserved." << std::endl << std::endl;
-
-    std::cout << "Remote SN: " + remoteSN << std::endl;
-    std::cout << "Local SN: " + localSN << std::endl;
-    std::cout << "License config file: " + licCfgPath << std::endl;
+    // Parse parameters
+    if (local_sn.empty() || remote_sn.empty() || path_to_lic_jsn.empty()) {
+        printHelp();
+        return 1;
+    }
 
     try {
-        // Create Omni-Teleop
-        flexiv::omni::teleop::Robot2RobotTeleop teleop(localSN, remoteSN, licCfgPath);
+        // TDK Initialization
+        // =========================================================================================
+        // Instantiate main teleop interface
+        flexiv::tdk::Robot2RobotTeleop teleop(local_sn, remote_sn, path_to_lic_jsn);
 
-        // Enable teleop robots
-        teleop.enable();
+        // Enable cart teleop
+        teleop.Enable();
 
-        // Init teleop robots
-        teleop.init();
+        // Init cart teleop
+        teleop.Init();
 
-        // Set preferred joint position to a better configuration
-        teleop.setLocalNullSpacePosture(k_preferredJntPos);
-        teleop.setRemoteNullSpacePosture(k_preferredJntPos);
+        // Set preferred joint position
+        teleop.SetLocalNullSpacePosture(kPreferredJntPos);
+        teleop.SetRemoteNullSpacePosture(kPreferredJntPos);
 
-        // Set max remote contact wrench
-        teleop.setRemoteMaxWrench(k_defaultMaxRemoteWrench);
+        // Set max contact wrench
+        teleop.SetMaxContactWrench(kDefaultMaxContactWrench);
 
         // Create real-time scheduler to step periodic tasks
-        flexiv::omni::teleop::Scheduler scheduler;
+        flexiv::rdk::Scheduler scheduler;
 
-        // Wait for elbow posture ready
+        // Wait for null-space posture ready
         std::this_thread::sleep_for(std::chrono::seconds(3));
 
-        // Add periodic task with 1ms interval and highest applicable priority
-        scheduler.AddTask(std::bind(&periodicTeleopTask, std::ref(teleop)), "HP periodic teleop", 1,
+        // Add periodic teleop task with 1ms interval and highest applicable priority
+        scheduler.AddTask(std::bind(&PeriodicTeleopTask, std::ref(teleop)), "HP periodic teleop", 1,
             scheduler.max_priority());
 
-        scheduler.AddTask(std::bind(&periodicConsoleTask, std::ref(teleop)), "LP Periodic console",
+        // Add periodic console task with 1s interval and lowest applicable priority
+        scheduler.AddTask(std::bind(&PeriodicConsoleTask, std::ref(teleop)), "LP Periodic console",
             1000, scheduler.min_priority());
 
         // Start all added tasks, this is by default a blocking method
         scheduler.Start();
 
-        std::cout << "Flexiv Omni-Teleop started ... ";
+        spdlog::info("Flexiv TDK example started ... ");
 
-        // Wait a bit for any last-second robot log message to arrive and get printed
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        // Block until signal received
+        // Block and wait for signal to stop scheduler tasks
         while (!g_stop_sched) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
+        // Received signal to stop scheduler tasks
+        scheduler.Stop();
 
     } catch (const std::exception& e) {
-        std::cerr << (e.what());
+        spdlog::error(e.what());
         return 1;
     }
 
