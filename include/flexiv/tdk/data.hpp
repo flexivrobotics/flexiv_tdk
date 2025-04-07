7afcc5ac1c15a5aa56dcdff252ebc5dec6f58702
Peizhang Zhu <peizhang.zhu@flexiv.com>
2025-04-07 16:51:26 -0700

    Update headers and Ubuntu x86_64 lib

--- include/flexiv/tdk/data.hpp
+++ include/flexiv/tdk/data.hpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <cstddef>
 #include <string>
+#include <mutex>
 
 namespace flexiv {
 namespace tdk {
@@ -26,7 +27,89 @@ constexpr size_t kIOPorts = 18;
 constexpr double kMaxWrenchFeedbackScale = 3;
 
 /**
- * @brief Reference coordinate that the axis to be locked
+ * @brief Motion control command struct for general device-robot teleop.
+ */
+struct MotionControlCmds
+{
+private:
+    /**
+     * @param pose Target TCP pose in world frame: \f$ {^{O}T_{TCP}}_{d} \in \mathbb{R}^{7
+     * \times 1} \f$. Consists of \f$ \mathbb{R}^{3 \times 1} \f$ position and \f$ \mathbb{R}^{4
+     * \times 1} \f$ quaternion: \f$ [x, y, z, q_w, q_x, q_y, q_z]^T \f$. Unit: \f$ [m]:[] \f$.
+     */
+    std::array<double, kPoseSize> pose {};
+    /**
+     * @param velocity Target TCP velocity (linear and angular) in world frame: \f$
+     * ^{0}\dot{x}_d \in \mathbb{R}^{6 \times 1} \f$. Providing properly calculated target velocity
+     * can improve the robot's overall tracking performance at the cost of reduced robustness.
+     * Leaving this input 0 can maximize robustness at the cost of reduced tracking performance.
+     * Consists of \f$ \mathbb{R}^{3 \times 1} \f$ linear and \f$ \mathbb{R}^{3 \times 1} \f$
+     * angular velocity. Unit: \f$ [m/s]:[rad/s] \f$.
+     */
+    std::array<double, kCartDoF> velocity {};
+
+    /**
+     * @param acceleration Target TCP acceleration (linear and angular) in world frame: \f$
+     * ^{0}\ddot{x}_d \in \mathbb{R}^{6 \times 1} \f$. Feeding forward target acceleration can
+     * improve the robot's tracking performance for highly dynamic motions, but it's also okay to
+     * leave this input 0. Consists of \f$ \mathbb{R}^{3 \times 1} \f$ linear and \f$
+     * \mathbb{R}^{3 \times 1} \f$ angular acceleration. Unit: \f$ [m/s^2]:[rad/s^2] \f$.
+     */
+    std::array<double, kCartDoF> acceleration {};
+
+    /**
+     * @brief mutex for thread safety.
+     */
+    mutable std::mutex mutex;
+
+public:
+    /**
+     * @brief Tread-safe read function: copy data to output parameters.
+     * @param[out] out_pose Target TCP pose in world frame.
+     * @param[out] out_velocity Target TCP velocity (linear and angular) in world frame.
+     * @param[out] out_acceleration Target TCP acceleration (linear and angular) in world frame.
+     */
+    void read(std::array<double, kPoseSize>& out_pose, std::array<double, kCartDoF>& out_velocity,
+        std::array<double, kCartDoF>& out_acceleration) const
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        out_pose = pose;
+        out_velocity = velocity;
+        out_acceleration = acceleration;
+    }
+
+    /**
+     * @brief Thread-safe write function: copy the input parameters to member variables.
+     * @param in_pose Target TCP pose in world frame.
+     * @param in_velocity Target TCP velocity (linear and angular) in world frame.
+     * @param in_acceleration Target TCP acceleration (linear and angular) in world frame.
+     */
+    void write(const std::array<double, kPoseSize>& in_pose,
+        const std::array<double, kCartDoF>& in_velocity,
+        const std::array<double, kCartDoF>& in_acceleration)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        pose = in_pose;
+        velocity = in_velocity;
+        acceleration = in_acceleration;
+    }
+
+    /**
+     * @brief Thread-safe zero function: set all data to 0.
+     */
+    void zero()
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        pose.fill(0);
+        // q.w() = 1;
+        pose[3] = 1;
+        velocity.fill(0);
+        acceleration.fill(0);
+    }
+};
+
+/**
+ * @brief Reference coordinate that the axis to be locked for high transparency teleop
  */
 enum CoordType
 {
@@ -38,7 +121,7 @@ enum CoordType
 static const std::string CoordTypeStr[] = {"UNKNOWN", "TCP", "WORLD"};
 
 /**
- * @brief Get the coordinate type of axis locking status
+ * @brief Get the coordinate type of axis locking status for high transparency teleop
  * @param[in] str string name of the coordinate
  * @return CoordType
  */
@@ -53,8 +136,9 @@ static inline CoordType GetCoordType(const std::string& str)
 }
 
 /**
- * @brief Data for locking axis, including reference frame and axis to be locked.
- * Coordinate type options are: "COORD_TCP" for TCP frame and "COORD_WORLD" for WORLD frame.
+ * @brief Data for locking axis, including reference frame and axis to be locked for high
+ * transparency teleop. Coordinate type options are: "COORD_TCP" for TCP frame and "COORD_WORLD" for
+ * WORLD frame.
  */
 struct AxisLock
 {
