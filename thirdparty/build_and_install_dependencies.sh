48083b7702ff16f45057dcc8e2472a7bddf036b4
ZhipengZhang-flexiv <18595628284@163.com>
2026-07-23 15:40:14 +0800

    remove jazzy option

--- thirdparty/build_and_install_dependencies.sh
+++ thirdparty/build_and_install_dependencies.sh
@@ -8,7 +8,6 @@ set -e
 # Initialize variables
 INSTALL_DIR=""
 NUM_JOBS=4
-BUILD_FOR_JAZZY=""
 
 # Function to print usage
 print_usage() {
@@ -20,11 +19,10 @@ print_usage() {
     echo ""
     echo "Options:"
     echo "  -j, --jobs <num>          Number of parallel build jobs (default: 4)."
-    echo "  --ros2-jazzy              Set BUILD_FOR_JAZZY=1 for ROS2 Jazzy environment."
     echo "  -h, --help                Show this help message."
     echo ""
     echo "Example:"
-    echo "  $0 /opt/flexiv --ros2-jazzy -j 8"
+    echo "  $0 /opt/flexiv -j 8"
 }
 
 # Parse arguments
@@ -33,11 +31,6 @@ while [ "$#" -gt 0 ]; do
         -h|--help)
             print_usage
             exit 0
-            ;;
-        --ros2-jazzy)
-            export BUILD_FOR_JAZZY=1
-            echo "Detected --ros2-jazzy flag: BUILD_FOR_JAZZY set to 1"
-            shift
             ;;
         -j|--jobs)
             if [ -z "$2" ] || [ "${2#-}" != "$2" ]; then
@@ -76,9 +69,6 @@ fi
 
 echo "Dependencies will be installed to: $INSTALL_DIR"
 echo "Number of parallel build jobs: $NUM_JOBS"
-if [ -n "$BUILD_FOR_JAZZY" ]; then
-    echo "ROS2 Jazzy mode enabled (BUILD_FOR_JAZZY=1)"
-fi
 
 # Clone all dependencies in a subfolder
 mkdir -p cloned && cd cloned
